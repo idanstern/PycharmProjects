3e4fe89f22527e42b9dd6ea4a9d4d3cf0870ae4d
idanstern <idanpc1@gmail.com>
2023-08-19 19:45:02 +0300

    another commit

--- 12.py
+++ 12.py
@@ -1,10 +1,10 @@
 my_file = open("names.txt")
 names = my_file.readlines()
 print(names)
-for name in names:
-    print(f"hello {name}",end='')
 
 my_file.close()
+
+print("aviel")
 
 my_file2 = open("names.txt", 'a+')
 for i in range(3):
